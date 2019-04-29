da5f0b1e31656f9801d8bc1385833feef0082f6a
kavitamakariye <kmakariye24@gmail.com>
2019-04-29 08:11:19 +0530

    code to access the weather api

---
+++ index.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const argv = require('yargs').argv;
+let city = argv.c || 'aurangabad';
+let apiKey = '853f8067646523ef7e35920829ec2b9e';
+//let city = 'aurangabad';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+request(url,function(err,response,body){
+    if(err){
+        console.log('error:',error);
+        
+    }
+    else{
+        //console.log('body:',body);
+        let weather = JSON.parse(body);
+let message = `It's ${weather.main.temp} degrees in ${weather.name
+}!`;
+console.log(message);
+    }
+});
