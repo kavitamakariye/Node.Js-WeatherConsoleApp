cf9df2530ad6eeb674874efac31a0659c6c4ad0e
kavitamakariye <kavitasmakariye@gmail.com>
2019-05-26 21:47:11 +0530

    modified file

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ request(url,function(err,response,body){
     else{
         //console.log('body:',body);
         let weather = JSON.parse(body);
-let message = `It's ${weather.main.temp} degrees in ${weather.name
-}!`;
+let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
 console.log(message);
     }
 });
